a37120207865825cb33e39ff0bb734f58cb688a0
IT21273858 <IT21273858@my.sliit.lk>
2024-05-08 05:37:39 +0530

    email validation added

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient, Prisma } = require('@prisma/client')
 const prisma = new PrismaClient();
 
 
@@ -10,45 +10,67 @@ const prisma = new PrismaClient();
 
 
 router.route('/admin/getAll').get((req, res) => {
-    prisma.user.findMany({
-        include: {
-            c_Instructor: {
-                include: {
-                    module: true
-                }
-            },
-            payment: true,
-            enrollment: true
-        }
-    }).then((data) => {
-        res.status(200)
-            .json({ status: true, message: "Admins retrieved successful", data });
+    try {
+        prisma.user.findMany({
+            include: {
+                c_Instructor: {
+                    include: {
+                        module: true
+                    }
+                },
+                payment: true,
+                enrollment: true
+            }
+        }).then((data) => {
+            res.status(200)
+                .json({ status: true, message: "Admins retrieved successful", data });
+        })
 
-    })
+    } catch (error) {
+        console.error("Error finding admin:", error);
+        res.status(500).json({ status: false, message: "Internal Server Error" });
+    }
 })
 
-router.route('/admin/create').post((req,res)=> {
-    const data={
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-        phoneNumber:req.body.phoneNumber,
-        Role:req.body.Role,
-        v_Status:req.body.v_Status
+router.route('/admin/create').post((req, res) => {
+    const data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        phoneNumber: req.body.phoneNumber,
+        Role: req.body.Role,
+        v_Status: req.body.v_Status
     }
-    prisma.user.create({
-       data
-    }).then((data) =>{
-        res.status(201).json({status:true,message:` ${data.Role} created Succesfully`, data})
-        console.log("User Created",data);
-    })
+    prisma.user.create({ data })
+        .then((data) => {
+            if (data) {
+                res.status(201).json({ status: true, message: `${data.Role} created successfully`, data });
+            } else {
+                res.status(400).json({ status: false, message: "Error creating admin" });
+            }
+            console.log("User Created", data);
+        })
+        .catch(error => {
+            console.error("Error occurred:", error);
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                // Handle Prisma known request errors
+                if (error.code === 'P2002') {
+                    res.status(500).json({ status: false, message: "New user cannot be created with this email", code:"P2002" });
+                }
+            } else {
+                res.status(500).json({ status: false, message: "An unexpected error occurred" });
+            }
+        });
 });
+
 
 router.route('/admin/update/:id').patch();
 
 router.route('/admin/delete/:id').delete();
 
-router.route('/admin/get/:id').get();
+router.route('/admin/get/:id').get((req, res) => {
+    const i = req.params.id
+});
 
 router.route('/admin/login').post();
 
